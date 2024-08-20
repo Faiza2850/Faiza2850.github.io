dcd78896b0de8037611965bcca377ffd73136a49
Faiza2850 <saffafatima06@gmail.com>
2024-08-20 17:08:40 +0500

    Faiza Portfolio

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ var typed = new Typed('#element', {
       strings: ['<i>Web Developer</i> ', 'Graphic Designer', 'Web Designer', 'Video Editor'],
       typeSpeed: 50,
     });
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+      event.preventDefault();
+      alert('Message sent successfully!');
+  });
